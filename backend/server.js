83cd272eb7557e42302b318346718d09aac03bef
Manmohit <manmohitgrandhi@gmail.com>
2024-11-27 19:43:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,14 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.log("Error connecting to MongoDB:", error));
 
+app.get("/" , (req,res) =>{
+    res.send('API is running')
+})
+
 app.use("/api/users", userRoutes);
 app.use("/api/roles", roleRoutes);
 
-app.get("/" , (req,res) =>{
-    res.sending('API is running')
-})
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
